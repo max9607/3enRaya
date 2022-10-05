efd1bc19e00f5de273daa9ff6df0146a49ec5ab8
Aldunate <sc.anthony.aldunate.j@upds.net.b>
2022-10-05 18:31:40 -0400

    Agregar archivos de proyecto.

--- 3enRaya/Program.cs
+++ 3enRaya/Program.cs
@@ -7,16 +7,20 @@ public class TresEnRaya1
 {
 
   
-    static int[,] tablero = new int[3, 3];
+    
+
     static char[] simbolo = { ' ', 'O', 'X' };
 
 
  
     public static void Main()
     {
-        bool terminado = false;
+        bool partida = false;
+        char[,] tablero = new char[3, 3] { { '-', '-', '-' }, { '-', '-', '-' }, { '-', '-', '-' } };
+        //int[,] tablero = new int[3, 3] { { 1,2,3}, { 4,5,6}, { 7,8,9 } };
+        DibujarTablero(tablero);
 
-      
+
         DibujarTablero();
         do
         {
@@ -34,9 +38,9 @@ public class TresEnRaya1
     }
  
 
-    static void DibujarTablero()
+    static void DibujarTablero(char[,] m)
     {
-        Console.WriteLine();
+        /*Console.WriteLine();
         Console.WriteLine("-------------");
         for (int fila = 0; fila < 3; fila++)
         {
@@ -45,6 +49,17 @@ public class TresEnRaya1
                 Console.Write(" {0} |", simbolo[tablero[fila, columna]]);
             Console.WriteLine();
             Console.WriteLine("-------------");
+        }*/
+        for(int i= 0; i < 3; i++)
+        {
+            for(int j= 0; j < 3; j++)
+            {
+                Console.Write("| ");
+                Console.Write(m[i,j]);
+            }
+            Console.Write(" |");
+            Console.WriteLine();
+            //hola
         }
     }
 
